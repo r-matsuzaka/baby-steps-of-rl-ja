365bd56effdace5ba69ad5db8695db41161f8c13
ryo-matsuzaka <matsuzakaryo@gmail.com>
2022-01-08 01:18:31 +0900

    feat:add docstring

--- DP/environment.py
+++ DP/environment.py
@@ -4,7 +4,13 @@ import numpy as np
 
 
 class State:
-    """状態を定義するクラス"""
+    """状態（位置）を定義するクラス
+    原点は左上
+    正の向き
+        -->
+        ↓
+
+    """
 
     def __init__(self, row=-1, column=-1):
         self.row = row
@@ -14,6 +20,12 @@ class State:
         return "<State: [{}, {}]>".format(self.row, self.column)
 
     def clone(self):
+        """
+        （現在の）状態(位置)を返す関数
+
+        Returns:
+            Stateクラス
+        """
         return State(self.row, self.column)
 
     #
@@ -53,7 +65,7 @@ class Environment:
         # Agent can move to a selected direction in move_prob.
         # It means the agent will move different direction
         # in (1 - move_prob).
-        self.move_prob = move_prob
+        self.move_prob = move_prob  # 同一方向
         self.reset()
 
     @property
@@ -88,6 +100,13 @@ class Environment:
 
     @property
     def states(self):
+        """
+        グリッド内のすべてのセルの状態(位置)を返す関数
+        ただし、ブロックセルは除く
+
+        Returns:
+            List: 各セルの状態(位置)のリスト
+        """
         states = []
         for row in range(self.row_length):
             for column in range(self.column_length):
@@ -98,11 +117,15 @@ class Environment:
 
     def transit_func(self, state, action):
         """遷移関数
+        次に移動し得る可能性の各グリッドの遷移確率を返す関数
+        関数transitに呼び出される
         Args:
-            state():
-            action():
+            state: 現在の位置
+            action: 移動を選択する方向
         Returns:
-            float: 遷移確率
+            dict: 次に移動し得る可能性の各グリッドの遷移確率
+                - ゴール地点にいるとき -> {}
+                - ゴール地点にいないとき、{次の位置:遷移確率、...}
         """
         transition_probs = {}
         if not self.can_action_at(state):
@@ -112,34 +135,59 @@ class Environment:
         opposite_direction = Action(action.value * -1)
 
         for a in self.actions:
-            print(a, action)
+            # 選択した方向の真逆の場合
             prob = 0
+
+            # 選択した方向の場合
             if a == action:
                 prob = self.move_prob
+
+            # 選択した方向の真逆でない場合
+            # 進行方向に対して、右か左
             elif a != opposite_direction:
                 prob = (1 - self.move_prob) / 2
 
             next_state = self._move(state, a)
+
             if next_state not in transition_probs:
+
                 transition_probs[next_state] = prob
+
             else:
                 transition_probs[next_state] += prob
 
         return transition_probs
 
     def can_action_at(self, state):
+        """
+        セルが0（移動可能なセル）かどうかを確認する関数
+
+        Returns:
+            bool: 0ならTrue, 0以外ならFalse
+        """
         if self.grid[state.row][state.column] == 0:
             return True
         else:
             return False
 
     def _move(self, state, action):
+        """
+        現在の状態から選択した方向に移動する関数
+        ただし、範囲外またはブロックセルの場合は現在の位置を返す
+
+        Args:
+            state: 現在の位置
+            action: 移動を選択する方向
+        Returns:
+            Stete: 現在の位置からの移動後の位置
+        """
         if not self.can_action_at(state):
             raise Exception("Can't move from here!")
 
         next_state = state.clone()
 
         # Execute an action (move).
+        # 正の向きに注意
         if action == Action.UP:
             next_state.row -= 1
         elif action == Action.DOWN:
@@ -150,13 +198,18 @@ class Environment:
             next_state.column += 1
 
         # Check whether a state is out of the grid.
+        # 範囲外の場合は、移動前の状態のままにする
         if not (0 <= next_state.row < self.row_length):
+            print("範囲外に移動しようとしました")
             next_state = state
         if not (0 <= next_state.column < self.column_length):
+            print("範囲外に移動しようとしました")
             next_state = state
 
         # Check whether the agent bumped a block cell.
+        # ブロックセルの場合は、移動前の状態のままにする
         if self.grid[next_state.row][next_state.column] == 9:
+            print("ブロックセルに移動しようとしました")
             next_state = state
 
         return next_state
@@ -164,10 +217,12 @@ class Environment:
     def reward_func(self, state):
         """報酬関数
         Args:
-
+            state(State()): 状態(位置)
         Returns:
-
+            float: 報酬。緑セル(1)なら+1を与える。赤セル(-1)なら-1を与える
+            bool:緑または赤セルにいる場合はTrue。それ以外の場合はFalse。
         """
+        # ゴール（赤or緑セル）以外のセルのときはデフォルトの報酬を与える
         reward = self.default_reward
         done = False
 
@@ -185,11 +240,18 @@ class Environment:
         return reward, done
 
     def reset(self):
+        """
+        初期状態（位置）を返す関数
+
+        Returns:
+            State: 初期状態（位置）
+        """
         # Locate the agent at lower left corner.
         self.agent_state = State(self.row_length - 1, 0)
         return self.agent_state
 
     def step(self, action):
+        """ """
         next_state, reward, done = self.transit(self.agent_state, action)
         if next_state is not None:
             self.agent_state = next_state
@@ -197,6 +259,16 @@ class Environment:
         return next_state, reward, done
 
     def transit(self, state, action):
+        """
+
+        Args:
+            State: 現在の位置
+            action: エージェントがランダムに決定した進行方向（実際の移動方向とは異なる）
+        Returns:
+            State: 次の位置
+            float: 次の位置で得られる報酬
+            bool: 次の位置がゴールの場合にTrue、それ以外の場合にFalse
+        """
         transition_probs = self.transit_func(state, action)
         if len(transition_probs) == 0:
             return None, None, True
@@ -207,6 +279,7 @@ class Environment:
             next_states.append(s)
             probs.append(transition_probs[s])
 
+        # 次の移動する可能性のある各セルの遷移確率に基づいて、次の移動先を選択
         next_state = np.random.choice(next_states, p=probs)
         reward, done = self.reward_func(next_state)
         return next_state, reward, done
--- DP/environment_demo.py
+++ DP/environment_demo.py
@@ -1,3 +1,13 @@
+# 単純にスタートからゴール地点まで移動するためだけのプログラム
+
+####################################### ルール #######################################
+# 各ステップに置いて上下左右に1セル動くことが可能
+# 各セルに移動すると報酬が与えられる。
+# ゴール地点に到達するまでの総報酬を計算する
+# ゴール地点が二箇所用意されている。緑(1)に到達すると+1が与えられる。赤(-1)に到達すると-1が与えられる。
+# それ以外の経路では、一律マイナスの報酬-0.04が与えられる
+# ブロックセル(9)またはグリッドの範囲外には移動できない。（移動しようとすると、元の状態(位置)からやり直し。）
+#####################################################################################
 import random
 
 from environment import Environment
@@ -7,11 +17,18 @@ class Agent:
     def __init__(self, env):
         self.actions = env.actions
 
-    def policy(self, state):
+    def policy(self):
+        """
+        ランダムに上下左右の移動方向を選択する関数
+
+        Returns:
+            int: 1(UP), -1(DOWN) , 2(LEFT), -2(RIGHT
+        """
         return random.choice(self.actions)
 
 
 def main():
+    """ """
     # Make grid environment.
     grid = [[0, 0, 0, 1], [0, 9, 0, -1], [0, 0, 0, 0]]
     env = Environment(grid)
@@ -22,21 +39,19 @@ def main():
         # Initialize position of agent.
         state = env.reset()
         print(f"#########################step{i}#########################")
-        print(repr(env.agent_state))
-        # プロパティの確認
-        print(f"row_length:{env.row_length}\n")
-        print(f"column_length:{env.column_length}\n")
-        print(f"actions:{env.actions}\n")
-        print(f"states:{env.states}\n")  # <State: [1, 1]>はブロックされているので、print出力されない
+        print(f"初期位置：{repr(env.agent_state)}")
 
         total_reward = 0
         done = False
 
+        # 赤(1)または緑(-1)のセルに到達するまで実行する
+        # 赤(1)または緑(-1)のセルに到達するとdoneはTrue (See reward_func)
         while not done:
-            action = agent.policy(state)
+            action = agent.policy()
             next_state, reward, done = env.step(action)
+            print(f"次の移動セル:{next_state}")
+            print(f"得られた報酬:{reward}")
             total_reward += reward
-            state = next_state
 
         print("Episode {}: Agent gets {} reward.".format(i, total_reward))
 
